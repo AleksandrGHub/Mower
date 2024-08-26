3d9b41417678645aba0226b0d797c40ffbc3375b
AleksandrGHub <bulanov.shura@gmail.com>
2024-08-26 06:25:09 +0300

    corriction 26-08-2024 6-25-05

--- Assets/Scripts/PathPointer.cs
+++ Assets/Scripts/PathPointer.cs
@@ -6,22 +6,22 @@ public class PathPointer : MonoBehaviour
 
     private Vector3[] _waypoints;
     public float _speed;
-    private int _currentWaypoint = 0;
+    private int _currentNumberWaypoint = 0;
 
     private void Start()
     {
         AddWaypoints();
-        transform.position = _waypoints[_currentWaypoint];
+        transform.position = _waypoints[_currentNumberWaypoint];
     }
 
     private void Update()
     {
-        if (transform.position == _waypoints[_currentWaypoint])
+        if (transform.position == _waypoints[_currentNumberWaypoint])
         {
-            _currentWaypoint = (++_currentWaypoint) % _waypoints.Length;
+            _currentNumberWaypoint = (++_currentNumberWaypoint) % _waypoints.Length;
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentWaypoint], _speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _waypoints[_currentNumberWaypoint], _speed * Time.deltaTime);
     }
 
     private void AddWaypoints()
